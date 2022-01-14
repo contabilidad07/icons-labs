9d375477d30185db5f42424eeae3535a5cd9e675
“Contabilidad07” <profecontabilidad07@gmail.com>
2022-01-13 22:33:20 -0300

    initial commit

---
+++ src/main/java/com/icons/icons/lab/IconsLabApplication.java
@@ -0,0 +1,13 @@
+package com.icons.icons.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IconsLabApplication.class, args);
+	}
+
+}
